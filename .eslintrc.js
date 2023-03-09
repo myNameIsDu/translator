ba162aebc6a19c3b768c6002736bde03b726fb7e
myNameIsDu <sunbohaoddd@gmail.com>
2023-03-09 14:08:34 +0800

    chore(root): 启用 typescript-eslint jest jest-dom testing-library eslint 插件

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,27 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "jest": true,
+    env: {
+        browser: true,
+        es2021: true,
+        jest: true,
     },
-    "extends": ["plugin:react/recommended","plugin:react/jsx-runtime"],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint",
-        "jest",
-        "jest-dom",
-        "testing-library"
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react/jsx-runtime',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:jest/recommended',
+        'plugin:jest/style',
+        'plugin:jest-dom/recommended',
+        'plugin:testing-library/dom',
     ],
-    "rules": {
-    }
-}
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['react', '@typescript-eslint', 'jest', 'jest-dom', 'testing-library'],
+    rules: {},
+};
