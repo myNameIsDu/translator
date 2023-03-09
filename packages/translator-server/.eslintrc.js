e093d9d931a4de9ac746255978faef37c93371e5
myNameIsDu <sunbohaoddd@gmail.com>
2023-03-09 15:01:37 +0800

    fix(root): 修复 prettier

--- packages/translator-server/.eslintrc.js
+++ packages/translator-server/.eslintrc.js
@@ -1,5 +1,5 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ["@remix-run/eslint-config", "@remix-run/eslint-config/node"],
-  root:true
+    extends: ['@remix-run/eslint-config', '@remix-run/eslint-config/node'],
+    root: true,
 };
