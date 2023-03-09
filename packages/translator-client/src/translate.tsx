10bb1e454d81177e33b22903a862ecad97a4949d
myNameIsDu <sunbohaoddd@gmail.com>
2023-03-09 14:09:48 +0800

    fix(client): 删除找不到词条时的log

--- packages/translator-client/src/translate.tsx
+++ packages/translator-client/src/translate.tsx
@@ -12,15 +12,6 @@ export function Translate({ text, id }: TranslatePropsType): JSX.Element {
     const { extra, holder } = holderFinder(text);
     const realId = id || holder;
 
-    if (!locales[realId] && process.env.NODE_ENV === 'development') {
-        console.log(
-            '%c' +
-                (realId === text
-                    ? `文案'${text}'未找到对应翻译`
-                    : `文案'${text}'(id: '${realId}') 未找到对应翻译`),
-            'color:red; font-weight: 600;',
-        );
-    }
     const localesText = locales[realId]?.[currentLang] || holder;
     const resultText = localesText.replace(matchHolderRegex, (_, $1) => extra[$1]);
 
